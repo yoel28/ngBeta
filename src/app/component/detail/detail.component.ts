1dcc8c925ac096540512c2d1ed693f1588009540
Yoel Rodriguez <yo3l.m18@gmail.com>
2020-08-18 20:53:10 -0500

    detail component has been included

--- src/app/component/detail/detail.component.ts
+++ src/app/component/detail/detail.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {IData} from '../../type';
+import {environment} from '../../../environments/environment';
+import {AgencyService} from '../../service/agency.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss']
+  styleUrls: ['./detail.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DetailComponent implements OnInit {
+  @Input() data: IData;
 
-  constructor() { }
+  public url: string;
+  constructor(private service: AgencyService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.url = environment.imgserver.replace(':id', this.getId());
+  }
+  public onSave($event: Event): void {
+    if ($event) {
+      $event.preventDefault();
+      $event.stopImmediatePropagation();
+    }
+    this.data.star = !this.data.star;
+    this.service.updateStar(this.data.index, this.data.star);
+  }
+
+  private getId(): string {
+    return Math.round(Math.random() * 50).toString();
   }
 
 }
