f708fe0b1831e044af842ff68e01fdb356ec3817
Yoel Rodriguez <yo3l.m18@gmail.com>
2020-08-18 20:53:35 -0500

    image serve has been included

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  imgserver: 'https://picsum.photos/id/:id/100/100',
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  imgserver: 'https://picsum.photos/id/:id/100/100',
   production: false
 };
 
